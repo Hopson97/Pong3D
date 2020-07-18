6998d811cc07c72b2adf0e0f0c14c153c38f3b49
Hopson97 <mhopson@hotmail.co.uk>
2020-07-18 15:39:38 +0100

    Add settings menu

--- src/ScreenMainMenu.cpp
+++ src/ScreenMainMenu.cpp
@@ -12,6 +12,19 @@ ScreenMainMenu::ScreenMainMenu(ScreenStack* stack)
 
 void ScreenMainMenu::onRender()
 {
+    switch (m_activeMenu) {
+        case ScreenMainMenu::Menu::MainMenu:
+            showMainMenu();
+            break;
+
+        case ScreenMainMenu::Menu::SettingsMenu:
+            showSettingsMenu();
+            break;
+    }
+}
+
+void ScreenMainMenu::showMainMenu()
+{
     if (imguiBeginCustom("M A I N   M E N U")) {
         ImGui::Text("Welcome to Pong 3D by Hopson");
         ImGui::Separator();
@@ -20,9 +33,21 @@ void ScreenMainMenu::onRender()
             m_pScreens->pushScreen(std::make_unique<ScreenInGame>(m_pScreens));
         }
         if (imguiButtonCustom("Settings")) {
+            m_activeMenu = Menu::SettingsMenu;
         }
         if (imguiButtonCustom("Exit Game")) {
             m_pScreens->popScreen();
+        }
+    }
+    ImGui::End();
+}
+
+void ScreenMainMenu::showSettingsMenu()
+{
+    if (imguiBeginCustom("S E T T I N G S")) {
+
+        if (imguiButtonCustom("Back")) {
+            m_activeMenu = Menu::MainMenu;
         }
     }
     ImGui::End();
--- src/ScreenMainMenu.h
+++ src/ScreenMainMenu.h
@@ -3,10 +3,18 @@
 #include "Screen.h"
 
 class ScreenMainMenu final : public Screen {
+    enum class Menu {
+        MainMenu,
+        SettingsMenu,
+    };
   public:
     ScreenMainMenu(ScreenStack* stack);
 
     void onRender() override;
 
   private:
+    Menu m_activeMenu = Menu::MainMenu;
+
+    void showMainMenu();
+    void showSettingsMenu();
 };
